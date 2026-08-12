f325c4e4e266c5132a27376c64403c7f867b9b1c
hepziba786-boop <hepziba786@gmail.com>
2026-08-13 04:06:14 +0530

    Configure ChromeDriver with options for headless testing
    Updated ChromeDriver initialization to use ChromeOptions for headless mode and other arguments.

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,13 +1,19 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 public class LoginTest {
 
     @Test
     void clickButton() {
 
-        ChromeDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+options.addArguments("--headless=new");
+options.addArguments("--no-sandbox");
+options.addArguments("--disable-dev-shm-usage");
+
+ChromeDriver driver = new ChromeDriver(options);
 
         driver.get("file:///app/index.html");
 
