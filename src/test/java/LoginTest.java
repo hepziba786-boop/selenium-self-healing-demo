b8f5b154a66c5f0b9017dcc6ae854330852ce8be
hepziba786-boop <hepziba786@gmail.com>
2026-08-13 12:24:37 +0530

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -13,9 +13,9 @@ public class LoginTest {
                 Paths.get("app/index.html")
         );
 
-assertTrue(
-    html.contains("submitBtn"),
-    "submitBtn not found"
-);
+        assertTrue(
+                html.contains("submitBtn"),
+                "submitBtn not found"
+        );
     }
 }
